b948eb08d60bceea4828d62e81a8ee12ddc83393
Nicklas Hafiz <hafiznij@hu-berlin.de>
2022-09-08 12:34:51 +0200

    added point labels

--- R/plot_points.R
+++ R/plot_points.R
@@ -1,7 +1,9 @@
 plot_points <- function(my_data){
   ggplot2::ggplot(data = my_data, aes(x = year, y = est, colour = adjust, group = adjust)) + #, shape = sig
     ggplot2::geom_point() +
+    geom_text(aes(label = round(est, 0)), nudge_y = c(15, -15), size = 3) +
     theme_line_iqb() +
     colour_iqb +
     scale_x_continuous(position = "top") # ins theme mit rein?
 }
+
