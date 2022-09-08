592eca088ba8df7982ef124fa1073ce2f9561526
Nicklas Hafiz <hafiznij@hu-berlin.de>
2022-09-08 10:09:53 +0200

    added working example for braces

--- R/draw_brace.R
+++ R/draw_brace.R
@@ -1,8 +1,5 @@
 draw_brace_small <- function(dat_long, range_est){
-
-# search for correct brace position - smallest est as reference
-
-brace_res <- ggbrace::geom_brace(
+  ggbrace::geom_brace(
     mapping = aes(
       x = c(2016, 2021),
       y = c(range_est[1] -62, range_est[1] - 92
@@ -12,36 +9,18 @@ brace_res <- ggbrace::geom_brace(
     rotate = 180,
     size = 0.8,
     npoints = 200,
-    labelsize = 2) #+
-  # ggbrace::geom_brace(
-  #   mapping = aes(
-  #     x = c(2011, 2021),
-  #     y = c(range_est[1] - 60, range_est[1] - 30),
-  #     label = "my_label"
-  #   ),
-  #   mid = 0.25,
-  #   inherit.data = F,
-  #   rotate = 180,
-  #   size = 0.8, npoints = 200) +
-    # coord_cartesian(ylim = c(range_est[1] - 30, range_est[2] + 30), clip = "off") + # limits without droping observations
-    # theme(plot.margin = unit(c(0, 0, 0.30, 0), units="npc")) +
-  #NULL
-
- #   return(brace_res)
+    labelsize = 2)
 }
 
-
 draw_brace_large <- function(dat_long, range_est){
-
-  # search for correct brace position - smallest est as reference
   ggbrace::geom_brace(
     mapping = aes(
       x = c(2011, 2021),
       y = c(range_est[1] - 60, range_est[1] - 30),
-      label = "my_label"
-    ),
+      label = " \n  my_label \n my_label2"),
     mid = 0.25,
     inherit.data = F,
     rotate = 180,
-    size = 0.8, npoints = 200)
+    size = 0.8, npoints = 200,
+    labelsize = 2)
 }
