f810b4fb51ed6534c152d7d0ec1188d35e65a26c
Nicklas Hafiz <hafiznij@hu-berlin.de>
2022-09-12 14:44:24 +0200

    label for large brace

--- R/draw_brace.R
+++ R/draw_brace.R
@@ -17,11 +17,13 @@ draw_brace_small <- function(dat_long, range_est){
 }
 
 draw_brace_large <- function(dat_long, range_est){
+  upper_label <- round(unique(dat_long %>% dplyr::filter(adjust == "mitAdj") %>% .$est_trend_2011.vs.2021_mean), 0)
+  lower_label <- round(unique(dat_long %>% dplyr::filter(adjust == "ohneAdj") %>% .$est_trend_2011.vs.2021_mean), 0)
   ggbrace::geom_brace(
     mapping = aes(
       x = c(2011, 2021),
       y = c(range_est[1] - 60, range_est[1] - 30),
-      label = " \n  my_label \n my_label2"),
+      label = paste0(upper_label, "\n", lower_label)),
     mid = 0.25,
     inherit.data = F,
     rotate = 180,
--- tests/testthat/test-plot_line.R
+++ tests/testthat/test-plot_line.R
@@ -54,7 +54,7 @@ NULL
 p3 <- p2 +
   coord_cartesian(ylim = c(range_est[1] - 30, range_est[2]), clip = "off") + # necessary, so the brace can be drawn inside the plot
   draw_brace_small(dat_long = dat_long, range_est = range_est) +
-  #draw_brace_large(dat_long  = dat_long, range_est = range_est) +
+  draw_brace_large(dat_long  = dat_long, range_est = range_est) +
   theme(plot.margin = unit(c(0, 0, 0.30, 0), units="npc")) +
   NULL
 
