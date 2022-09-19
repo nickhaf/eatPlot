2c3b2869ed298b60431cad47342c0426bd68e41f
Nicklas Hafiz <hafiznij@hu-berlin.de>
2022-09-19 15:36:57 +0200

    changed label indention

--- R/plot_points.R
+++ R/plot_points.R
@@ -7,7 +7,7 @@ plot_points <- function(my_data, grouping_var){
                       shape = sig)
                   ) +
     ggplot2::geom_point(size = 2.3) +
-    geom_text(aes(label = round(est, 0)), nudge_y = c(15, -15), size = 3) +
+    geom_text(aes(label = round(est, 0)), nudge_y = c(-15, 15), size = 3) +
     theme_line_iqb() +
     grouping_colours +
     scale_x_continuous(position = "top") # ins theme mit rein?
