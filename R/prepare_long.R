1c24cab9e279b72d07f1f45c57e9f22c48c60edf
Nicklas Hafiz <hafiznij@hu-berlin.de>
2022-09-21 15:08:53 +0200

    own data set for point estimates

--- R/prepare_long.R
+++ R/prepare_long.R
@@ -1,24 +1,25 @@
-bt21 <- read.csv2("Q:/BT2021/BT/60_Bericht/06_Soziale_Disparitäten/Abbildungen/01_KAS/Nicklas/Daten/Abb65Buecher_mitTrend_KOPIE.csv")
+bt21 <- read.csv2("Q:/BT2021/BT/60_Bericht/06_Soziale_Disparitäten/Abbildungen/01_KAS/Nicklas/Daten/Abb65Buecher_mitTrend_KOPIE.csv", na.strings = "")
 
-wide_bt <- prepare_wide(bt21, competence = "GL")
+prepare_long <- function(wide_bt, grouping_var){
 
-prepare_long <- function(wide_bt){
-
-  long_bt <- wide_bt %>%
-    filter(is.na(comparison)) %>%
-    filter(!is.na(KBuecher_imp3)) %>%
-    filter(!(group %in% c("0","1"))) %>%
+  data_long <- wide_bt %>%
+  # filter(is.na(comparison)) %>%
+  # filter(!is.na(.[[grouping_var]])) %>%
+  # filter(!(group %in% c("0","1"))) %>%
     gather(parameter, estimate, c(est_2011:est_2021,
                                   p_2011:p_2021,
                                   se_2011:se_2021)
     ) %>%
     separate(parameter, c("parameter", "year")) %>%
     spread(parameter, estimate) %>%
-    #select(kb, adjust, TR_BUNDESLAND, year, est, p, se )
-    mutate(year = as.numeric(year)) %>%
-    mutate(sig = ifelse(p < 0.05, "Sig", "noSig"))
+    mutate(parameter = case_when(is.na(comparison) ~ "mean_group",
+                                 ))
 
-  for(i in unique(bt21a$TR_BUNDESLAND)){
-    bt21a_long[grepl(i, bt21a_long$group),"TR_BUNDESLAND"] <- i
-  }
+
+  data_long <- data_long[,c("kb", "TR_BUNDESLAND", grouping_var, "year", "est", "p")] %>%
+    rename(p_est = p) %>%
+    mutate(year = as.numeric(year)) %>%
+    mutate(sig = ifelse(p_est < 0.05, "Sig", "noSig"))
+
+  return(data_long)
 }
--- tests/testthat/test-plot_line-sozDis.R
+++ tests/testthat/test-plot_line-sozDis.R
@@ -4,85 +4,18 @@ library(tidyverse)
 ## Create necessary data sets: 1 long Trendset, and one long point estimate set
 
 
-bt21 <- read.csv2("Q:/BT2021/BT/60_Bericht/06_Soziale_Disparitäten/Abbildungen/01_KAS/Nicklas/Daten/Abb65Buecher_mitTrend_KOPIE.csv")
-colnames(bt21) <- gsub("sig", "p", colnames(bt21))
+bt21 <- read.csv2("Q:/BT2021/BT/60_Bericht/06_Soziale_Disparitäten/Abbildungen/01_KAS/Nicklas/Daten/Abb65Buecher_mitTrend_KOPIE.csv", na.strings = "")
 
-bt21a <- bt21 %>%
-  filter(parameter == "mean") %>%
-  filter(kb == "GL")
+bt21_prep <- prepare_general(bt21)
+bt21_pointEstimates <- prepare_pointEstimates(bt21_prep, competence = "GL", grouping_var = "KBuecher_imp3")
 
-
-for(i in unique(bt21a$TR_BUNDESLAND)[-grepl("", unique(bt21a$TR_BUNDESLAND))]){
-  for(j in c("2011", "2016", "2021")){
-  bt21a[grepl(i, bt21a$group),"TR_BUNDESLAND"] <- i
-  # Eintragen der p-Werte in die entsprechende Zeile
-  bt21a[grepl(paste0(i, "_0"), bt21a$group), paste0("p_", j)] <- bt21a[grepl(paste0(i, "_0", ".vs.wholeGroup"), bt21a$group), paste0("p_",j )]
-}
-}
-
-bt21a <- bt21a %>%
-  filter(!is.na(TR_BUNDESLAND) | group %in% c("0", "1"))
-
-
-bt21a_long <- bt21a %>%
-  filter(is.na(comparison)) %>%
-  filter(!is.na(KBuecher_imp3)) %>%
-  filter(!(group %in% c("0","1"))) %>%
-  gather(parameter, estimate, c(est_2011:est_2021,
-                                p_2011:p_2021,
-                                se_2011:se_2021)
-  ) %>%
-  separate(parameter, c("parameter", "year")) %>%
-  spread(parameter, estimate) %>%
-  #select(kb, adjust, TR_BUNDESLAND, year, est, p, se )
-  mutate(year = as.numeric(year)) %>%
-  mutate(sig = ifelse(p < 0.05, "Sig", "noSig"))
-
-for(i in unique(bt21a$TR_BUNDESLAND)){
-  bt21a_long[grepl(i, bt21a_long$group),"TR_BUNDESLAND"] <- i
-}
-
+bundeslaender <- unique(bt21_pointEstimates$TR_BUNDESLAND)
 
 plot_list <- list()
-min_est <- min(bt21a_long$est)
 range_est <- range(bt21a_long$est)
 position <- 1
 
-
-
-# Finally, plot whole Germany
-if(position > length(unique(bt21a$TR_BUNDESLAND))){
-  dat_germany <- bt21 %>%
-    filter(group %in% c("0", "1"), parameter == "mean", kb == "GL")
-
-}
-
-
-
-for(i in unique(bt21a$TR_BUNDESLAND)){
-  dat_long <- bt21a_long %>%
-    filter(TR_BUNDESLAND == i)
-
-  ## Trenddatensatz bauen --> trennen von Trend und Punktestimates. Außerdem year_start und year_end als eigene Spalten
-  dat_trend <- dat_long %>%
-    select(group, KBuecher_imp3, est_trend_2016.vs.2021, est_trend_2011.vs.2021, p_trend_2016.vs.2021, p_trend_2011.vs.2021) %>%
-    rename(estTrend_2016vs2021 = est_trend_2016.vs.2021,
-           estTrend_2011vs2021 = est_trend_2011.vs.2021,
-           pTrend_2016vs2021 = p_trend_2016.vs.2021,
-           pTrend_2011vs2021 = p_trend_2011.vs.2021) %>%
-    gather(parameter, estimate, c(estTrend_2011vs2021, estTrend_2016vs2021,
-                                  pTrend_2011vs2021, pTrend_2016vs2021)) %>%
-    unique %>%
-    separate(parameter, c("parameter", "trendyears")) %>%
-    separate(col = trendyears, into = c("year_start", "year_end"), sep = "vs") %>%
-    mutate(year_start = as.numeric(year_start),
-           year_end = as.numeric(year_end)) %>%
-    spread(parameter, estimate) %>%
-    mutate(sig_trend_2016 = ifelse(pTrend < 0.05, "bold", "plain")) %>%
-    mutate(brace_upper_y = ifelse(year_start == min(year_start), range_est[1] - 30, range_est[1] -62)) %>%
-    mutate(brace_lower_y = ifelse(year_start == min(year_start), range_est[1] - 60, range_est[1] -92)) %>%
-    mutate(label_pos = ifelse(KBuecher_imp3 == 1, range_est[1] - 103, range_est[1] - 123))
-
+for(i in bundeslaender){
 
   bt21a_sig <- bt21a %>%
     filter(kb == "GL",
@@ -96,10 +29,11 @@ for(i in unique(bt21a$TR_BUNDESLAND)){
     mutate(sig_2016.vs.2021 = ifelse(p_trend_2016.vs.2021 < 0.05, "Sig", "noSig")) %>%
     mutate(sig = "")
 
-  whole_group <- bt21 %>% filter(group == "wholeGroup", parameter == "mean", kb == "GL") %>%
+  whole_group <- bt21 %>%
+    filter(group == "wholeGroup", parameter == "mean", kb == "GL") %>%
     mutate(sig = "")
 
-    p1 <- ggplot2::ggplot(data = dat_long) +
+    p1 <- ggplot2::ggplot(data = bt21_pointEstimates) +
     geom_segment(data = whole_group,
                  aes(
                    x = rep(as.numeric(2011), nrow(whole_group)),
@@ -120,7 +54,7 @@ for(i in unique(bt21a$TR_BUNDESLAND)){
                  size = 0.7,
                  color = rgb(147, 205, 221,
                              maxColorValue = 255)) +
-    plot_points(my_data = dat_long, grouping_var = "KBuecher_imp3")+
+    plot_points(my_data = dat_long, grouping_var = "KBuecher_imp3") +
     scale_shape_manual(values = c(16, 17, 16)) # Nochmal anschauen!
 
 
