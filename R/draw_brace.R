f8b89a497930d7ee5f583997e55fa9dc305483b7
Nicklas Hafiz <hafiznij@hu-berlin.de>
2022-10-31 12:25:28 +0100

    improved bracelabel position

--- R/draw_brace.R
+++ R/draw_brace.R
@@ -7,7 +7,7 @@ draw_brace <- function(dat_trend, upper_label, lower_label, year_vec, bundesland
   brace_coordinates <- dat_trend %>%
     mutate(brace_upper_y = ifelse(year_start == min(year_start), range_est[1] - 40, range_est[1] -72)) %>%
     mutate(brace_lower_y = ifelse(year_start == min(year_start), range_est[1] - 70, range_est[1] -102)) %>%
-    mutate(label_pos = ifelse(KBuecher_imp3 == 1, range_est[1] - 113, range_est[1] - 143)) %>%
+    mutate(label_pos = ifelse(KBuecher_imp3 == 1, range_est[1] - 113, range_est[1] - 133)) %>%
     mutate(sigTrend_vsGermany = ifelse(pTrend_vsGermany < 0.05, "<sup>a</sup>", "")) %>%
     mutate(estTrend_within_label = ifelse(sigTrend_within == "bold", paste0("**", round(estTrend_within, 0), "**"), round(estTrend_within, 0)))
 
@@ -41,7 +41,7 @@ draw_brace <- function(dat_trend, upper_label, lower_label, year_vec, bundesland
                                          !(brace_coordinates$year_start == 2011 &
                                             brace_coordinates$year_end == 2016), ],
               mapping = aes(
-                x = year_start + (year_end - max(year_start))/2 - 0.45 ,
+                x = year_start + (year_end - max(year_start))/2 ,
                 y = label_pos,
                 label = paste0(estTrend_within_label, sigTrend_vsGermany, " (", round(seTrend_within, 1), ")")
                 ),
