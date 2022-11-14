293cbacfe07d2fb1a92c13fff4dabaa19e040db0
Nicklas Hafiz <hafiznij@hu-berlin.de>
2022-11-14 14:55:36 +0100

    styled blot_bar

--- R/plot_bar.R
+++ R/plot_bar.R
@@ -19,22 +19,22 @@ plot_bar <- function(my.dat){
       x = est,
       y = group,
       pattern_fill = adjust
-      ),
-      position = position_dodge(width = 0.8),
-      color = "black",
-      size = 0.6,
-      pattern_colour = "white",
-      pattern_angle = - 45,
-      pattern_density = 0.4, #Streifenbreite
-      pattern_spacing = 0.01, #Abstand
-      pattern_key_scale_factor = 0.6,
-      width = 0.4) +
+    ),
+    position = position_dodge(width = 0.8),
+    color = "black",
+    size = 0.6,
+    pattern_colour = "white",
+    pattern_angle = - 45,
+    pattern_density = 0.4, #Streifenbreite
+    pattern_spacing = 0.01, #Abstand
+    pattern_key_scale_factor = 0.6,
+    width = 0.4) +
 
     ggpattern::scale_pattern_manual(values = c(yes = "none",
-                                    no = "stripe")) +
+                                               no = "stripe")) +
 
     ggpattern::scale_pattern_fill_manual(values = c("ohneAdj" = rgb(147, 205, 221, maxColorValue = 255),
-                                         "mitAdj" = rgb(33, 89, 104, maxColorValue = 255))) +
+                                                    "mitAdj" = rgb(33, 89, 104, maxColorValue = 255))) +
 
     ggplot2::scale_x_continuous(breaks = seq(-40, 40, by = 10)) +
     fill_iqb_adj_sig +
