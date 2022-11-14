fa704368cccfca3b14ca36ea99dab372150e877c
Nicklas Hafiz <hafiznij@hu-berlin.de>
2022-11-14 14:52:51 +0100

    cleanup test_plot_table

--- tests/testthat/test-plot_table.R
+++ tests/testthat/test-plot_table.R
@@ -1,7 +1,17 @@
-t1 <- bt21_NoTrend_prep %>%
+bt21_NoTrend <- readxl::read_excel("Q:/BT2021/BT/60_Bericht/04_Mittelwerte/adjustierte Mittelwerte/Ergebnisse/01_ohneTrend_z_standard.xlsx")
+
+t1 <- bt21_NoTrend %>%
+  mutate(significant = factor(ifelse(p < 0.05, "yes", "no"))) %>%
+  mutate(adj_sig = case_when(
+    adjust == "ohneAdj" & significant == "no" ~ "noAdj_noSig",
+    adjust == "ohneAdj" & significant == "yes" ~ "noAdj_Sig",
+    adjust == "mitAdj" & significant == "no" ~ "Adj_noSig",
+    adjust == "mitAdj" & significant == "yes" ~ "Adj_Sig"
+  )) %>%
   filter(kb == "GL" & is.na(comparison) & group != "wholeGroup") %>%
   select(group, est, adjust) %>%
   as.data.frame %>%
   reshape(idvar = "group", timevar = "adjust" , direction = "wide", v.names = c("est"))
 
+p1 <- plot_bar(bt21_NoTrend_prep)
 grid.table(t1, theme = t1_theme)
