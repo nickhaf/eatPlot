6123f339b52ce38f61b08b61863b1bdf53bb9c14
Nicklas Hafiz <hafiznij@hu-berlin.de>
2022-11-14 14:45:30 +0100

    styled merge_trend_point

--- R/merge_trend_point.R
+++ R/merge_trend_point.R
@@ -1,25 +1,24 @@
-merge_trend_point <- function(trend1, trend2, point, grouping_var){
-
+merge_trend_point <- function(trend1, trend2, point, grouping_var) {
   merged_trend <- merge(trend1, trend2,
-                       by = c("TR_BUNDESLAND", grouping_var, "year_start", "year_end")
-                       )
-
+    by = c("TR_BUNDESLAND", grouping_var, "year_start", "year_end")
+  )
 
   trend_merged <- merge(merged_trend,
-                       point,
-                       by.x = c("TR_BUNDESLAND", "year_start", grouping_var),
-                       by.y = c("TR_BUNDESLAND", "year", grouping_var),
-                       all.x = TRUE,
-                       sort = FALSE)
-colnames(trend_merged)[colnames(trend_merged) == "est"] <- "est_start"
+    point,
+    by.x = c("TR_BUNDESLAND", "year_start", grouping_var),
+    by.y = c("TR_BUNDESLAND", "year", grouping_var),
+    all.x = TRUE,
+    sort = FALSE
+  )
+  colnames(trend_merged)[colnames(trend_merged) == "est"] <- "est_start"
 
   trend_merged2 <- merge(trend_merged,
-                        point[,c("TR_BUNDESLAND", "KBuecher_imp3", "year", "est")],
-                        by.x = c("TR_BUNDESLAND", "KBuecher_imp3", "year_end"),
-                        by.y = c("TR_BUNDESLAND", "KBuecher_imp3", "year"),
-                        all.x = TRUE,
-                        sort = FALSE)
+    point[, c("TR_BUNDESLAND", "KBuecher_imp3", "year", "est")],
+    by.x = c("TR_BUNDESLAND", "KBuecher_imp3", "year_end"),
+    by.y = c("TR_BUNDESLAND", "KBuecher_imp3", "year"),
+    all.x = TRUE,
+    sort = FALSE
+  )
   colnames(trend_merged2)[colnames(trend_merged2) == "est"] <- "est_end"
   return(trend_merged2)
-
 }
