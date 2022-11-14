e30dd8e2b9190bc861b3deb65cc06773acc061d3
Nicklas Hafiz <hafiznij@hu-berlin.de>
2022-11-14 14:11:30 +0100

    styled colour_schemes

--- R/colour_schemes.R
+++ R/colour_schemes.R
@@ -4,46 +4,53 @@
 # Filling colours ---------------------------------------------------------
 
 fill_iqb <- ggplot2::scale_fill_manual(
-
-  values = c("ohneAdj" = rgb(147, 205, 221,
-                             maxColorValue = 255
-                             ),
-             "mitAdj"  = rgb(33, 89, 104,
-                             maxColorValue = 255
-                             )
+  values = c(
+    "ohneAdj" = rgb(147, 205, 221,
+      maxColorValue = 255
+    ),
+    "mitAdj" = rgb(33, 89, 104,
+      maxColorValue = 255
+    )
   )
 )
 
 
 fill_iqb_adj_sig <- ggplot2::scale_fill_manual(
-
-  values = c("noAdj_Sig" = rgb(147, 205, 221,
-                               maxColorValue = 255),
-             "Adj_Sig" = rgb(33, 89, 104, maxColorValue = 255),
-             "noAdj_noSig" = "white",
-             "Adj_noSig" = "white"
+  values = c(
+    "noAdj_Sig" = rgb(147, 205, 221,
+      maxColorValue = 255
+    ),
+    "Adj_Sig" = rgb(33, 89, 104, maxColorValue = 255),
+    "noAdj_noSig" = "white",
+    "Adj_noSig" = "white"
   )
 )
 
 
 # Scale colours -----------------------------------------------------------
 
-colour_iqb <- ggplot2::scale_colour_manual(values = c("ohneAdj" = rgb(147, 205, 221, maxColorValue = 255),
-                                                      "mitAdj" = "black")) # Gruppen immer als 1 und 0
+colour_iqb <- ggplot2::scale_colour_manual(values = c(
+  "ohneAdj" = rgb(147, 205, 221, maxColorValue = 255),
+  "mitAdj" = "black"
+)) # Gruppen immer als 1 und 0
 
-grouping_colours <- ggplot2::scale_colour_manual(values = c("0" = rgb(166, 166, 166, maxColorValue = 255),
-                                                            "1" = "black"))
+grouping_colours <- ggplot2::scale_colour_manual(values = c(
+  "0" = rgb(166, 166, 166, maxColorValue = 255),
+  "1" = "black"
+))
 
 
 
 # Shapes ------------------------------------------------------------------
 
 ## Linetype: solid and dashed
-linetype_iqb <- ggplot2::scale_linetype_manual(values=c("bold" = 1,
-                                                        "plain" = 2)
-)
+linetype_iqb <- ggplot2::scale_linetype_manual(values = c(
+  "bold" = 1,
+  "plain" = 2
+))
 
 ## Pointshape: triangle and circle
-pointshape_iqb <- ggplot2::scale_shape_manual(values=c("Sig" = 17,
-                                                       "noSig" = 16)
-)
+pointshape_iqb <- ggplot2::scale_shape_manual(values = c(
+  "Sig" = 17,
+  "noSig" = 16
+))
--- R/connect_points.R
+++ R/connect_points.R
@@ -3,23 +3,22 @@
 ## Ajdusts linetype according to significance of the within Trend variable.
 
 
-connect_points <- function(my_data, grouping_var ){
-
+connect_points <- function(my_data, grouping_var) {
   list(
-
-  plot_res <- geom_segment(data = my_data,
-                           aes(
-                             x = year_start,
-                             xend = year_end,
-                             y =  est_start,
-                             yend = est_end,
-                             colour = .data[[grouping_var]],
-                             linetype = sigTrend_within),
-                           size = 0.7),
-  linetype_iqb
-
+    plot_res <- ggplot2::geom_segment(
+      data = my_data,
+      aes(
+        x = year_start,
+        xend = year_end,
+        y = est_start,
+        yend = est_end,
+        colour = .data[[grouping_var]],
+        linetype = sigTrend_within
+      ),
+      size = 0.7
+    ),
+    linetype_iqb
   )
 
   return(plot_res)
 }
-
