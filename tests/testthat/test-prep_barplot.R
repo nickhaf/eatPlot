36cc8e1cd513354afe7a4d43ad3ea35d5cfeed99
Nicklas Hafiz <hafiznij@hu-berlin.de>
2023-01-12 10:27:50 +0100

    update tests

--- tests/testthat/test-prep_barplot.R
+++ tests/testthat/test-prep_barplot.R
@@ -1,10 +1,9 @@
 test_that("significance niveau is working correctly", {
 
-  df <- prep_barplot(adjusted_means_diff, subGroups = "adjust", sigNiveau = 0.02)
+  df <- prep_barplot(adjusted_means, subGroups = "adjust", sigNiveau = 0.02)
 
   expect_equal(df$p < 0.02, as.logical(df$sig))
   expect_false(isTRUE(all.equal(df$p < 0.05, as.logical(df$sig))))
 
 })
 
-test_that("")
