cc51f61c866942e42bbdda3ab396e8b4a92f71b2
Nicklas Hafiz <hafiznij@hu-berlin.de>
2023-03-16 11:24:12 +0100

    aligned y axis with plots

--- R/plot_y_axis.R
+++ R/plot_y_axis.R
@@ -1,6 +1,9 @@
 plot_y_axis <- function(plot_data) {
 
   range_est <- range(plot_data[["plot_points"]]$est_point, na.rm = TRUE)
+  coords <- calc_coords(range_est)
+
+
 
   df_y <- data.frame(trend = "20112016",
                      x = min(plot_data[["plot_points"]]$year),
@@ -14,22 +17,17 @@ plot_y_axis <- function(plot_data) {
     ggplot2::geom_segment(
       data = df_y,
       ggplot2::aes(
-        x = .data$x, #- (.data$xmax - .data$x) * 0.1, ## calc expansion of scale_x_continous
-        xend = .data$x, #- (.data$xmax - .data$x) * 0.1, ## calc expansion of scale_x_continous
+        x = .data$x,
+        xend = .data$x,
         y = .data$y,
         yend = .data$yend
       )
     ),
     ggplot2::scale_x_continuous(limits = c(min(plot_data[["plot_points"]]$year), min(plot_data[["plot_points"]]$year) + 1), expand = c(0,0)),
     theme_line(),
-    ggplot2::scale_y_continuous(
-      breaks = seq(
-        from = round(min(plot_dat[["plot_points"]]$est_point, na.rm = TRUE) - 10, -1),
-        to = round(max(plot_dat[["plot_points"]]$est_point, na.rm = TRUE), -1),
-        by = 20
-      ),
-      limits = c(calc_coords(range_est)[1], calc_coords(range_est)[2])
-    ),
+    set_y_coords(plot_data),
+    ## Use same coordinate system as the braces, so the plots can be aligned.
+    set_cartesian_coords(coords),
     ggplot2::theme(
       axis.text.y = ggplot2::element_text(),
       axis.ticks.y = ggplot2::element_line(),
--- R/set_plot_coords.R
+++ R/set_plot_coords.R
@@ -3,22 +3,30 @@ set_plot_coords <- function(plot_dat) {
   max_year <- max(plot_dat[["plot_points"]]$year, na.rm = TRUE)
 
   list(
-    ggplot2::scale_y_continuous(
-      breaks = seq(
-        from = round(min(plot_dat[["plot_points"]]$est_point, na.rm = TRUE) - 10, -1),
-        to = round(max(plot_dat[["plot_points"]]$est_point, na.rm = TRUE), -1),
-        by = 20
-      ),
-      expand = c(0, 0)
-    ),
+    set_y_coords(plot_dat),
     ggplot2::scale_x_continuous(
-      # limits = c(
-      #   min_year - (max_year - min_year) * 0.1,
-      #   max_year + (max_year - min_year) * 0
-      # ),
       position = "top",
       breaks = unique(plot_dat[["plot_points"]]$year),
       expand = c(0, 0)
     )
   )
+}
+
+
+set_y_coords <- function(plot_dat){
+  ggplot2::scale_y_continuous(
+    breaks = seq(
+      from = round(min(plot_dat[["plot_points"]]$est_point, na.rm = TRUE) - 10, -1),
+      to = round(max(plot_dat[["plot_points"]]$est_point, na.rm = TRUE), -1),
+      by = 20
+    ),
+    expand = c(0, 0)
+  )
+}
+
+set_cartesian_coords <- function(coords){
+ggplot2::coord_cartesian(
+  clip = "off", # Clip Coordinate system. Necessary, so the brace can be drawn under the x-axis.
+  ylim = coords
+)
 }
