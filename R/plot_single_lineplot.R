e216a9a3b7ffc822c7c90e222d53e9a1fa0b034f
Nicklas Hafiz <hafiznij@hu-berlin.de>
2023-04-12 11:37:27 +0200

    deal with NULL statement for linevalues

--- R/plot_single_lineplot.R
+++ R/plot_single_lineplot.R
@@ -39,19 +39,21 @@ if(background_lines == TRUE){
       line_values = line_values
     )
   },
+if(!is.null(line_values)){
     plot_lines(
       data_plot_lines = plot_data[["plot_lines"]],
       line_values = line_values,
       line_sig = line_sig,
       plot_settings = plot_settings
-    ),
+    )},
+if(!is.null(point_values)){
     plot_points(
       data_plot_points = plot_data[["plot_points"]],
       point_values = point_values,
       point_sig = point_sig,
       y_range = y_range,
       plot_settings = plot_settings
-    ),
+    )},
     plot_x_axis(plot_data[["plot_points"]],
       y_range = y_range,
       plot_settings = plot_settings
