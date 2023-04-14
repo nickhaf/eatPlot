70ef78e1617b9564e2f966297a5df32049fa1fdb
Nicklas Hafiz <hafiznij@hu-berlin.de>
2023-04-14 13:40:43 +0200

    function for background stripes

--- R/plot_table_continous.R
+++ R/plot_table_continous.R
@@ -5,57 +5,64 @@ plot_tablebar <- function(dat,
                           columns_table = NULL,
                           columns_table_sig_bold = NULL,
                           columns_table_sig_high = NULL,
-                          columns_table_se = NULL){
+                          columns_table_se = NULL,
+                          plot_settings = plotsettings_tablebarplot()) {
+  new_colnames <- paste0("col_", 1:length(columns_table))
 
-
-new_colnames <- paste0("col_", 1:length(columns_table))
-
-for(i in seq_along(columns_table)){
-  dat[, new_colnames[i]] <- construct_label(
-    dat,
-                              label_est = columns_table[[i]],
-                              label_se = columns_table_se[[i]],
-                              label_sig_bold = columns_table_sig_bold[[i]],
-                              label_sig_high = columns_table_sig_high[[i]]
-                              )
-}
-
-
-res_plot <- ggplot2::ggplot(
-    data = dat,
-  )
-
-if(!is.null(column_bar)){
-  res_plot <- res_plot +
-    ggpattern::geom_rect_pattern(
-      ggplot2::aes(
-        xmin = x_min,
-        xmax = x_max,
-        ymin = y_axis - 0.4,
-        ymax = y_axis + 0.4
-      ),
-      pattern = "stripe",
-      linetype = 2,
-      colour = "red"
+  for (i in seq_along(columns_table)) {
+    dat[, new_colnames[i]] <- construct_label(
+      dat,
+      label_est = columns_table[[i]],
+      label_se = columns_table_se[[i]],
+      label_sig_bold = columns_table_sig_bold[[i]],
+      label_sig_high = columns_table_sig_high[[i]]
     )
+  }
+
+
+  res_plot <- ggplot2::ggplot(
+    data = dat
+  ) +
+    build_background_stripes(dat, plot_settings = plot_settings) +
+    ggplot2::scale_y_continuous(expand = c(0, 0))
+
+
+  if (!is.null(column_bar)) {
+    res_plot <- res_plot +
+      ggpattern::geom_rect_pattern(
+        ggplot2::aes(
+          xmin = x_min,
+          xmax = x_max,
+          ymin = y_axis - 0.4,
+          ymax = y_axis + 0.4
+        ),
+        pattern = "stripe",
+        linetype = 2,
+        colour = "red"
+      )
+  }
+
+  if (any(!is.null(columns_table))) {
+    res_plot <- res_plot +
+      build_columns_2(dat, cols = rev(new_colnames)) +
+      # column headers:
+      ggplot2::geom_rect(
+        ggplot2::aes(
+          xmin = -Inf, xmax = Inf,
+          ymin = 4.5, ymax = Inf
+        ),
+        colour = "lightblue",
+        fill = "lightblue"
+      ) +
+      ggplot2::annotate("text", x = 0, y = 4.8, label = "Header") +
+      ggplot2::annotate("text", x = -3.5, y = 4.8, label = "Colspanner") +
+      ggplot2::annotate("segment", x = -4.5, xend = -2.5, y = 4.7, yend = 4.7) +
+      ggplot2::scale_x_continuous(breaks = c(-2, 0, 2), expand = c(0, 0))
+  }
+
+  return(res_plot)
 }
 
-if(any(!is.null(columns_table))){
-  res_plot <- res_plot +
-    build_columns_2(dat, cols = rev(new_colnames)) +
-    # column headers:
-    ggplot2::geom_rect(ggplot2::aes(xmin = -Inf, xmax = Inf,
-                                    ymin = 4.5, ymax = Inf),
-                       colour = "lightblue",
-                       fill = "lightblue") +
-    ggplot2::annotate("text", x = 0, y = 4.8, label = "Header") +
-    ggplot2::annotate("text", x = -3.5, y = 4.8, label = "Colspanner") +
-    ggplot2::annotate("segment", x = -4.5, xend = -2.5, y = 4.7, yend = 4.7) +
-    ggplot2::scale_x_continuous(breaks = c(-2, 0, 2), expand = c(0,0))
-}
-
-return(res_plot)
-}
 
 
 build_columns_2 <- function(df, cols) {
@@ -72,7 +79,7 @@ build_columns_2 <- function(df, cols) {
           y = y_axis,
           label = .data[[column_name]]
         ),
-        #size = plot_settings$brace_label_size,
+        # size = plot_settings$brace_label_size,
         label.padding = grid::unit(rep(0, 4), "pt"),
         fill = NA,
         label.color = NA,
@@ -82,7 +89,28 @@ build_columns_2 <- function(df, cols) {
   )
 }
 
-## Jede Spalte einzeln vorliegend mit variierendem label
-# verschiedene Plots dann mit patchwork verbinden
-# Manuelle Funktion mit geom_rect für die stripes.
-# Berechnen der Spaltenbreite (evtl von stringmenge abhängig machen?), auf alle Fälle manuell justierbar lassen.
+build_background_stripes <- function(dat,
+                                     plot_settings = plotsettings_tablebarplot()) {
+  dat$background_colour <- rev(plot_settings$background_stripes_colour)
+
+
+  stripes <- c(
+    ggplot2::geom_tile(
+      data = dat,
+      ggplot2::aes(
+        x = x_min,
+        y = y_axis,
+        width = Inf,
+        height = 1,
+        colour = background_colour,
+        fill = background_colour
+      )
+    ),
+    ggplot2::scale_fill_manual(
+      breaks = dat$background_colour,
+      values = dat$background_colour
+    ),
+    ggplot2::scale_colour_manual(values = dat$background_colour)
+  )
+  return(stripes)
+}
--- tests/testthat/test-plot_table_continous.R
+++ tests/testthat/test-plot_table_continous.R
@@ -1,28 +1,21 @@
+test_that("continous barplot looks the same", {
+  test_data <- data.frame(
+    y_axis = 1:4,
+    x_min = rep(0, 4),
+    x_max = c(1, -2, 0.4, 3),
+    est_1 = c("12", "12", "15", "23"),
+    se_1 = c("12", "10", "8", "4"),
+    sig_2 = c(TRUE, FALSE, TRUE, FALSE)
+  )
 
-# Versuch eines Barplots mit continous scale ------------------------------
+  p_bar <- plot_tablebar(dat = test_data,
+                columns_table = list("est_1", "se_1" ),
+                columns_table_sig_bold = list(NULL, "sig_2"),
+                columns_table_sig_high = list("sig_2", "sig_2"),
+                column_bar = "est_1",
+                plot_settings = plotsettings_tablebarplot(background_stripes_colour = c("white", "lightgrey", "white", "darkgrey")))
 
+  vdiffr::expect_doppelganger("Minimal tablebar", p_bar)
 
-# min_stand <- readxl::read_xlsx("Q:/BT2022/BT/60_Bericht/_Probegrafiken/2023-01-26 Vorlagen Balken und Linien/BT2021_Abb3.9.xlsx", sheet = "Daten BT21")
-# data_plot_new <- prep_trend(min_stand, competence = "lesen", parameter = "1")$plot_bar
+})
 
-## Nötig wäre: sehr breiter Plot, der eine durchgezogene Skala hat. Die Bars werden dann irgendwo geplotted, und daneben wird annotated
-
-## Liste mit Einstellungen für jede Spalte.
-
-#
-# test_data <- data.frame(
-#   y_axis = 1:4,
-#   x_min = rep(0, 4),
-#   x_max = c(1, -2, 0.4, 3),
-#   est_1 = c("12", "12", "15", "23"),
-#   se_1 = c("12", "10", "8", "4"),
-#   sig_2 = c(TRUE, FALSE, TRUE, FALSE)
-# )
-#
-#
-#
-# plot_tablebar(dat = test_data,
-#                columns_table = list("est_1", "se_1" ),
-#                columns_table_sig_bold = list(NULL, "sig_2"),
-#                columns_table_sig_high = list("sig_2", "sig_2"),
-#                column_bar = "est_1")
