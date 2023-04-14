bdd5d10fce5c7464c9c12dbaeb812fc417a502f3
Nicklas Hafiz <hafiznij@hu-berlin.de>
2023-04-14 12:27:57 +0200

    build table labels

--- R/plot_table_continous.R
+++ R/plot_table_continous.R
@@ -1,13 +1,23 @@
 
 
 plot_tablebar <- function(dat,
-                           column_bar = NULL,
-                           columns_table = NULL,
-                           columns_table_sig_bold = NULL){
+                          column_bar = NULL,
+                          columns_table = NULL,
+                          columns_table_sig_bold = NULL,
+                          columns_table_sig_high = NULL,
+                          columns_table_se = NULL){
 
 
-for(i in columns_table){
-  dat[, i] <- construct_label()
+new_colnames <- paste0("col_", 1:length(columns_table))
+
+for(i in seq_along(columns_table)){
+  dat[, new_colnames[i]] <- construct_label(
+    dat,
+                              label_est = columns_table[[i]],
+                              label_se = columns_table_se[[i]],
+                              label_sig_bold = columns_table_sig_bold[[i]],
+                              label_sig_high = columns_table_sig_high[[i]]
+                              )
 }
 
 
@@ -32,7 +42,7 @@ if(!is.null(column_bar)){
 
 if(any(!is.null(columns_table))){
   res_plot <- res_plot +
-    build_columns_2(test_data, cols = rev(columns_table)) +
+    build_columns_2(dat, cols = rev(new_colnames)) +
     # column headers:
     ggplot2::geom_rect(ggplot2::aes(xmin = -Inf, xmax = Inf,
                                     ymin = 4.5, ymax = Inf),
@@ -48,30 +58,6 @@ return(res_plot)
 }
 
 
-
-# ggplot2::ggplot(
-#   data = test_data,
-# ) +
-#   ggpattern::geom_rect_pattern(
-#     ggplot2::aes(
-#       xmin = x_min,
-#       xmax = x_max,
-#       ymin = y_axis - 0.4,
-#       ymax = y_axis + 0.4
-#     ),
-#     pattern = "stripe",
-#     linetype = 2,
-#     colour = "red"
-#   ) +
-#   build_columns_2(test_data, cols = c("group", "group_2")) +
-#   ggplot2::geom_rect(ggplot2::aes(xmin = -Inf, xmax = Inf, ymin = 4.5, ymax = Inf), colour = "lightblue", fill = "lightblue") +
-#   ggplot2::annotate("text", x = 0, y = 4.8, label = "Header") +
-#   ggplot2::annotate("text", x = -3.5, y = 4.8, label = "Colspanner") +
-#   ggplot2::annotate("segment", x = -4.5, xend = -2.5, y = 4.7, yend = 4.7) +
-#   ggplot2::scale_x_continuous(breaks = c(-2, 0, 2), expand = c(0,0))
-
-
-
 build_columns_2 <- function(df, cols) {
   x_axis <- min(c(df$x_min, df$x_max))
 
@@ -79,13 +65,18 @@ build_columns_2 <- function(df, cols) {
     lapply(1:length(cols), function(i) {
       column_name <- cols[i]
       x_axis <- x_axis - i # Das muss natürlich Skalenabhängig sein, und skalierbar
-      ggplot2::geom_text(
+      ggtext::geom_richtext(
         data = df,
         ggplot2::aes(
           x = x_axis,
           y = y_axis,
           label = .data[[column_name]]
-        )
+        ),
+        #size = plot_settings$brace_label_size,
+        label.padding = grid::unit(rep(0, 4), "pt"),
+        fill = NA,
+        label.color = NA,
+        hjust = 1
       )
     })
   )
--- tests/testthat/test-plot_table_continous.R
+++ tests/testthat/test-plot_table_continous.R
@@ -1,14 +1,14 @@
-#
-# # Versuch eines Barplots mit continous scale ------------------------------
-#
-#
+
+# Versuch eines Barplots mit continous scale ------------------------------
+
+
 # min_stand <- readxl::read_xlsx("Q:/BT2022/BT/60_Bericht/_Probegrafiken/2023-01-26 Vorlagen Balken und Linien/BT2021_Abb3.9.xlsx", sheet = "Daten BT21")
 # data_plot_new <- prep_trend(min_stand, competence = "lesen", parameter = "1")$plot_bar
-#
-# ## Nötig wäre: sehr breiter Plot, der eine durchgezogene Skala hat. Die Bars werden dann irgendwo geplotted, und daneben wird annotated
-#
-# ## Liste mit Einstellungen für jede Spalte.
-#
+
+## Nötig wäre: sehr breiter Plot, der eine durchgezogene Skala hat. Die Bars werden dann irgendwo geplotted, und daneben wird annotated
+
+## Liste mit Einstellungen für jede Spalte.
+
 #
 # test_data <- data.frame(
 #   y_axis = 1:4,
@@ -21,6 +21,8 @@
 #
 #
 #
-# plot_table_bar(dat = test_data,
-#                columns_table = c("est_1", "se_1" ),
+# plot_tablebar(dat = test_data,
+#                columns_table = list("est_1", "se_1" ),
+#                columns_table_sig_bold = list(NULL, "sig_2"),
+#                columns_table_sig_high = list("sig_2", "sig_2"),
 #                column_bar = "est_1")
