14c2801c86a9a4c9b753c7e2b11609493efb3b01
nickhaf <nicklashaf@gmail.com>
2023-04-22 12:32:27 +0200

    fixed merge_trend_point test

--- tests/testthat/test-merge_trend_point.R
+++ tests/testthat/test-merge_trend_point.R
@@ -16,15 +16,15 @@ test_that("merging with grouping_var", {
       state_var = rep(c("Berlin", "Brandenburg"), 6),
       grouping_var = rep(c(0, 0, 1, 1), 3),
       year = c(rep(2011, 4), rep(2013, 4), rep(2015, 4)),
-      est_point = 10:21,
+      point_values_noTrend_noComp = 10:21,
       depVar = c("a", "a"),
       competence_var = "b"
     )
 
   test_prep_df <- merge_trend_point(trend_data_1, point_data_1)
 
-  expect_equal(test_prep_df$point_values_start, c(10:17))
-  expect_equal(test_prep_df$point_values_end, c(14:21))
+  expect_equal(test_prep_df$point_values_noTrendstart_noComp, c(10:17))
+  expect_equal(test_prep_df$point_values_noTrendend_noComp, c(14:21))
   expect_equal(test_prep_df$year_start, c(rep(2011, 4), rep(2013, 4)))
   expect_equal(test_prep_df$year_end, c(rep(2013, 4), rep(2015, 4)))
 
