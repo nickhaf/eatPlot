07b5be369f18819e0cf602a6b367c3a65bd1865c
nickhaf <nicklashaf@gmail.com>
2023-04-30 17:18:25 +0200

    export calc_MinStand

--- R/calc_MinStand.R
+++ R/calc_MinStand.R
@@ -1,3 +1,13 @@
+#' Calculate the significance descriptions for the 'Mindeststandards'
+#'
+#' @param dat_bar Data prepared by `prep_plot()`.
+#' @param sig_column Column that contains the significances.
+#' @param est_column Column that contains the estimates.
+#'
+#' @return Dataframe with added `sig_minstand` column.
+#' @export
+#'
+#' @examples #tbd
 calc_MinStand <- function(dat_bar, sig_column, est_column){
 
   dat_bar$sig_minstand <- ifelse(dat_bar[, sig_column] == "TRUE" & dat_bar[, est_column] < 0,
