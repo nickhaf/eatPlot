44db511452999be5e9815e068cc58389dd8b9093
Nicklas Hafiz <hafiznij@hu-berlin.de>
2023-05-09 19:14:57 +0200

    start function for filtering rows from data.frame

---
+++ R/filter_plot_dat.R
@@ -0,0 +1,17 @@
+#' Filter specified rows from plot_dat objects.
+#'
+#' @param plot_dat A list of data.frames prepared by [prep_plot()].
+#' @param filter_statement Character string containing a logical expression for filtering specific rows in all data.frames in the list when possible. Write `dat$column_name` to specify the wanted columns.
+#' @param list_elements Character vector with the names of the list objects you want to filter in. Defaults to `c("plot_lines", "plot_points", "plot_background_lines", "plot_braces")`.
+#'
+#' @return The plot_dat list with rows filtered as specified.
+#' @export
+#'
+#' @examples
+filter_plot_dat <- function(plot_dat,
+                        filter_statement,
+                        list_elements = c("plot_lines", "plot_points", "plot_background_lines", "plot_braces")){
+  lapply(plot_dat[list_elements], function(dat){
+      dat[eval(parse(text = filter_statement)),]
+})
+}
