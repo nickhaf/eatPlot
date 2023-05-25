34acfee2ef0b060839c91b72c60d3d8b832e74c8
Nicklas Hafiz <hafiznij@hu-berlin.de>
2023-05-25 09:06:03 +0200

    save_plot is now basically ggsave with some useful defaults

--- R/save_plot.R
+++ R/save_plot.R
@@ -1,22 +1,24 @@
-#' Wrapper for ggsave.
+#' Wrapper for [ggplot2::ggsave()].
 #'
 #' @param p Plot that should be saved.
 #' @param filename Filepath and -name for the new plot file.
 #' @param width Width of the new plot file in mm. Defaults to `160`.
 #' @param height Height of the new plot file in mm. Defaults to `226.2`
 #' @param format Format of the new plot file. Currently, only PDF is needed.
-#' @param scaling Proportional width and height changes, so the proportion between them is kept.
+#' @param device Device for saving the plot. Should be `cairo_pdf` for pdfs, so font is displayed correctly.
+#' @param ... Other arguments passed on to [ggplot2::ggsave()].
 #'
 #' @return A PDF file.
 #' @export
 #'
 #' @examples # tbd
-save_plot <- function(p, filename, width = 160, height = 226.2, scaling = 1) {
+save_plot <- function(p, filename, width = 160, height = 226.2, device = cairo_pdf, ...) {
   ggplot2::ggsave(filename,
     p,
-    height = height * scaling,
-    width = width * scaling,
+    height = height,
+    width = width,
     units = "mm",
-    device = cairo_pdf
+    device = device,
+    ...
   )
 }
