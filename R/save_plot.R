e78c81ffb934e38ab695f6c17c2c6ebee76c04df
Nicklas Hafiz <hafiznij@hu-berlin.de>
2023-05-25 09:00:53 +0200

    use ggsave for saving plot, so correct dashes are saved

--- R/save_plot.R
+++ R/save_plot.R
@@ -1,4 +1,4 @@
-#' Title
+#' Wrapper for ggsave.
 #'
 #' @param p Plot that should be saved.
 #' @param filename Filepath and -name for the new plot file.
@@ -11,11 +11,12 @@
 #' @export
 #'
 #' @examples # tbd
-save_plot <- function(p, filename, width = 160, height = 226.2, format = "PDF", scaling = 1) {
-  width_inch <- width / 25.4
-  height_inch <- height / 25.4
-
-  grDevices::pdf(file = filename, width = width_inch * scaling, height = height_inch * scaling)
-  plot(p)
-  grDevices::dev.off()
+save_plot <- function(p, filename, width = 160, height = 226.2, scaling = 1) {
+  ggplot2::ggsave(filename,
+    p,
+    height = height * scaling,
+    width = width * scaling,
+    units = "mm",
+    device = cairo_pdf
+  )
 }
