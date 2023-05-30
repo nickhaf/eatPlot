e569e71c4cfe3413a06fd867c96f761a3333c360
Nicklas Hafiz <hafiznij@hu-berlin.de>
2023-05-30 17:38:55 +0200

    use grDevices instead of ggsave

--- R/save_plot.R
+++ R/save_plot.R
@@ -11,13 +11,29 @@
 #' @export
 #'
 #' @examples # tbd
-save_plot <- function(p, filename, width = 160, height = 226.2, device = grDevices::cairo_pdf, ...) {
-  ggplot2::ggsave(filename,
-    p,
-    height = height,
-    width = width,
-    units = "mm",
-    device = device,
-    ...
-  )
+
+## Cario.pdf device for correct dash representation:
+# save_plot <- function(p, filename, width = 160, height = 226.2, device = grDevices::cairo_pdf, ...) {
+#   ggplot2::ggsave(filename,
+#     p,
+#     height = height,
+#     width = width,
+#     units = "mm",
+#     device = device,
+#     colormodel = "cmyk"
+#   )
+# }
+
+## pdf for the correct colomodel cmyk:
+save_plot <- function(p, filename, width = 160, height = 226.2, format = "PDF", scaling = 1) {
+  width_inch <- width / 25.4
+  height_inch <- height / 25.4
+
+  grDevices::pdf(file = filename,
+                 width = width_inch * scaling,
+                 height = height_inch * scaling,
+                 colormodel = "cmyk"
+                 )
+  plot(p)
+  grDevices::dev.off()
 }
