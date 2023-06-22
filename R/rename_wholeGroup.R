7f72014fdbc49c395f089bdbdc17741a02ec9ce9
Nicklas Hafiz <hafiznij@hu-berlin.de>
2023-06-22 16:57:11 +0200

    function for renaming wholeGroup, closes #261

---
+++ R/rename_wholeGroup.R
@@ -0,0 +1,16 @@
+rename_wholeGroup <- function(plot_dat, whole_group = "wholeGroup", new_whole_group = "Deutschland") {
+  if (inherits(plot_dat, "list")) {
+    for (i in names(plot_dat)) {
+      plot_dat[[i]][] <- lapply(plot_dat[[i]], function(x) {
+        gsub(whole_group, new_whole_group, x)
+      })
+    }
+  } else if (is.data.frame(plot_dat)) {
+    plot_dat[] <- lapply(plot_dat, function(x) {
+      gsub(whole_group, new_whole_group, x)
+    })
+  } else {
+    stop("Please provide a list or a data.frame.")
+  }
+  return(plot_dat)
+}
