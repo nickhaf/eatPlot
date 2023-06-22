7c4be9f76d7e745d5dd49e11201e53dd39ced217
Nicklas Hafiz <hafiznij@hu-berlin.de>
2023-06-22 17:01:30 +0200

    rename rename_whole_group() to gsub_plot_dat()

--- R/gsub_plot_dat.R
+++ R/gsub_plot_dat.R
@@ -1,13 +1,15 @@
-rename_wholeGroup <- function(plot_dat, whole_group = "wholeGroup", new_whole_group = "Deutschland") {
+gsub_plot_dat <- function(plot_dat, whole_group = "wholeGroup", new_whole_group = "Deutschland") {
   if (inherits(plot_dat, "list")) {
     for (i in names(plot_dat)) {
       plot_dat[[i]][] <- lapply(plot_dat[[i]], function(x) {
-        gsub(whole_group, new_whole_group, x)
+        if(is.character(x)){gsub(whole_group, new_whole_group, x)
+          }else{x}
       })
     }
   } else if (is.data.frame(plot_dat)) {
     plot_dat[] <- lapply(plot_dat, function(x) {
-      gsub(whole_group, new_whole_group, x)
+      if(is.character(x)){gsub(whole_group, new_whole_group, x)
+        }else{x}
     })
   } else {
     stop("Please provide a list or a data.frame.")
