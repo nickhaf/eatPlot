37e4d5859229191d1062a0008cb4614cf5ffc5f6
Nicklas Hafiz <hafiznij@hu-berlin.de>
2023-07-25 18:18:41 +0200

    tests for filter_plot_dat, closes #442

--- tests/testthat/test-filter_plot_dat.R
+++ tests/testthat/test-filter_plot_dat.R
@@ -1,21 +1,21 @@
 test_that("multiplication works", {
   test_list <- list(
     "a" = data.frame(
-      col_1 = c(1, 2),
+      col_1 = c(1, NA),
       col_2 = c("a", "b")
     ),
     "b" = data.frame(
       col_1 = c(1, 2),
-      col_2 = c("a", "b")
+      col_2 = c("a", "a")
     )
   )
 
   expect_equal(
-    filter_plot_dat(test_list, filter_statement = "dat$col_1 == 1", list_elements = c("a", "b")),
+    filter_plot_dat(test_list, filter_statement = "dat$col_1 == 1 | is.na(dat$col_1)", list_elements = c("a", "b")),
     list(
       "a" = data.frame(
-        col_1 = c(1),
-        col_2 = c("a")
+        col_1 = c(1, NA),
+        col_2 = c("a", "b")
       ),
       "b" = data.frame(
         col_1 = c(1),
