4306bfc35c7cd461d82f8e4d93388d4b054a9100
Nicklas Hafiz <nicklashaf@gmail.com>
2024-12-27 18:32:58 +0100

    Rename `dat` to `eatRep_dat` in `prep_tablebarplot()`

--- R/prep_tablebarplot.R
+++ R/prep_tablebarplot.R
@@ -3,7 +3,7 @@ check_eatRep_dat(eatRep_dat)
 
 check_columns(eatRep_dat$plain, c("unit_1", "unit_2", "id", "comparison", "parameter", "year", "est", "se", "p", "mhg"))
 
-dat <- dat$plain
+dat <- eatRep_dat$plain
 dat <- dat[dat$parameter %in% par, ]
 dat$sig <- ifelse(dat$p < 0.05, TRUE, FALSE)
 
