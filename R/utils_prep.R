03f833f7a59ea9f46b4286cd7161c7b1d84e64fd
Nicklas Hafiz <nicklashaf@gmail.com>
2025-02-12 10:56:41 +0100

    Reorder column names

--- R/utils_prep.R
+++ R/utils_prep.R
@@ -47,7 +47,6 @@ merge_eatRep <- function(eatRep_unnested, eatRep_dat){
                      by.y = "id"
   )
 
-
   dat_group_est <- merge(dat_group,
                          eatRep_dat$estimate[, c("id", "est", "se", "p", "es", "sig", "parameter")] ,
                          by.x = "value",
@@ -56,9 +55,9 @@ merge_eatRep <- function(eatRep_unnested, eatRep_dat){
   dat_group_long <- merge(dat_group_est,
                           eatRep_dat$estimate[, c("id", "est", "se", "p", "es", "sig", "parameter")],
                           by = "id",
-                          suffixes = c("_comparison_none", "_comparison"))
+                          suffixes = c("_comp_none", "_comp"))
   if(nrow(eatRep_dat$comparisons) == 0){
-  dat_group_long[, grep("_comparison$", colnames(dat_group_long))] <- NA
+  dat_group_long[, grep("_comp$", colnames(dat_group_long))] <- NA
   }
 
   dat_group_long_t <- do.call(rbind, lapply(split(dat_group_long, dat_group_long$id), create_trend))
@@ -95,6 +94,7 @@ prep_comparisons <- function(eatRep_merged, facet_var, total_facet, total_subgro
 
 
       x$comparison_split <- paste0(x$comparison_split, "_", res, "Subgroup")
+
     }
 
     # Remove all comparisons that start with total! They are duplicates
@@ -104,40 +104,48 @@ prep_comparisons <- function(eatRep_merged, facet_var, total_facet, total_subgro
     if(any(grepl(total_subgroup, x$subgroup_var)) & length(unique(x$subgroup_var)) != 1){
       x <- x[x$subgroup_var != total_subgroup, ]
     }
+
     return(x)
   })
 
   dat_comp <- do.call(rbind, df_list)
-
   return(dat_comp)
 }
 
 pivot_eatRep <- function(eatRep_prepped, names_from){
-  value_cols <- c("est_comparison", "se_comparison", "p_comparison", "es_comparison", "sig_comparison")
+  value_cols <- c("est_comp", "se_comp", "p_comp", "es_comp", "sig_comp")
 
-  eatRep_prepped <- eatRep_prepped[, colnames(eatRep_prepped) %in% c("state_var", "subgroup_var", "kb", "year", "trend") | grepl("comparison|parameter", colnames(eatRep_prepped))]
+  eatRep_prepped <- eatRep_prepped[, colnames(eatRep_prepped) %in% c("state_var", "subgroup_var", "kb", "year", "trend") | grepl("comp|parameter", colnames(eatRep_prepped))]
 
   ## Split into comparions und comparison_none
-  eatRep_prepped_none <- eatRep_prepped[,  colnames(eatRep_prepped) %in% c("state_var", "subgroup_var", "kb", "year", "trend") | grepl("comparison_none", colnames(eatRep_prepped))]
-  eatRep_prepped_comp <- eatRep_prepped[,  colnames(eatRep_prepped) %in% c("state_var", "subgroup_var", "kb", "trend", "comparison_split") | grepl("comparison$", colnames(eatRep_prepped))]
+  eatRep_prepped_none <- eatRep_prepped[,  colnames(eatRep_prepped) %in% c("state_var", "subgroup_var", "kb", "year", "trend") | grepl("comp_none", colnames(eatRep_prepped))]
+  eatRep_prepped_comp <- eatRep_prepped[,  colnames(eatRep_prepped) %in% c("state_var", "subgroup_var", "kb", "trend", "comparison_split") | grepl("comp$", colnames(eatRep_prepped))]
 
   ## Die jetzt gesondert nach wide ziehen?
   eatRep_prepped_none_wide <-  tidyr::pivot_wider(
     unique(subset(eatRep_prepped_none, select = -c(trend))),
-    names_from = c("parameter_comparison_none", "year"),
+    names_from = c("year", "parameter_comp_none"),
     values_from = paste0(value_cols, "_none")
   )
 
   eatRep_prepped_comp_wide <-  tidyr::pivot_wider(
     unique(eatRep_prepped_comp[, colnames(eatRep_prepped_comp) != "comparison"]),
-    names_from = c("parameter_comparison", "trend", "comparison_split"),
+    names_from = c("comparison_split", "trend", "parameter_comp"),
     values_from = value_cols
   )
 
 ## year und trend, das muss noch geändert werden
-  trend_wide <- merge(eatRep_prepped_none_wide, eatRep_prepped_comp_wide)
+  eatPlot_dat <- merge(eatRep_prepped_none_wide, eatRep_prepped_comp_wide)
+colnames(eatPlot_dat)[grep("_comp_", colnames(eatPlot_dat))] <- vapply(colnames(eatPlot_dat)[grep("_comp_", colnames(eatPlot_dat))], function(col){
+    parameter <- unlist(regmatches(col, gregexpr("[^_]+$", col)))
+    col <- gsub(paste0("_", parameter, "$"), "", col)
+    col <- sub("_comp_", paste0("_", parameter, "_comp_"), col)
+    return(col)
+    },
+    FUN.VALUE = character(1))
 
-  return(trend_wide)
+
+  return(eatPlot_dat)
 }
 
 
