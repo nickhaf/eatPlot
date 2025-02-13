2b0fb65fa88293e322d87eed54c8a0402fe0f86f
Nicklas Hafiz <nicklashaf@gmail.com>
2025-02-13 11:30:23 +0100

    Remove background_subgroup warning in plot_lineplot()

--- R/plot_lineplot.R
+++ R/plot_lineplot.R
@@ -34,7 +34,7 @@ plot_lineplot <- function(eatPlot_dat,
                           years_lines = list(),
                           years_braces = list(),
                           background_facet = "total",
-                          background_subgroup = NULL,
+                          background_subgroup = "total",
                           box_facet = NULL,
                           title_superscripts = NULL,
                           plot_settings = plotsettings_lineplot()) {
@@ -49,10 +49,6 @@ plot_lineplot <- function(eatPlot_dat,
     warning("Split lineplot currently not supported. Set to non-split.")
     plot_settings$split_plot <- FALSE
   }
-
-if(is.null(background_subgroup)){
-  message("You haven't set a background_subgroup. This might lead to unexpected behaviour, if actually one group should be plotted in the background. Most times this will be the 'total' group.")
-}
 
   # Rename/Build needed columns ---------------------------------------------
   years_list <- prep_years_list(years_lines, years_braces)
