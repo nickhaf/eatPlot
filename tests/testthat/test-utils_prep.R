4661f8db7ea884661a32eaa54e8ff6fca6d440cc
Nicklas Hafiz <hafiznij@hu-berlin.de>
2025-04-30 10:42:03 +0200

    Change coding of Gender in example data

--- tests/testthat/test-utils_prep.R
+++ tests/testthat/test-utils_prep.R
@@ -11,12 +11,12 @@ test_that("Value in the prepped columns can be traced back to plain data frame (
 
 
   expect_equal(nrow(prepped_dat_berlin), 3)
-  expect_equal(subset(prepped_dat_berlin, subgroup_var == "weiblich", select = est_mean_comp_crossDiff_totalFacet_totalSubgroup_2009)[[1]],
+  expect_equal(subset(prepped_dat_berlin, subgroup_var == "female", select = est_mean_comp_crossDiff_totalFacet_totalSubgroup_2009)[[1]],
                subset(original_dat_berlin,
                       comparison == "crossDiff" &
                       TR_BUNDESLAND == "Berlin - total" &
                       parameter == "mean" &
-                      Kgender == "weiblich - total" &
+                      Kgender == "female - total" &
                       year == 2009,
                       select = "est"
                       )[[1]])
@@ -32,49 +32,49 @@ test_that("Value in the prepped columns can be traced back to plain data frame (
                )[[1]])
 
 
-  expect_equal(subset(prepped_dat_berlin, subgroup_var == "maennlich", select = se_mean_comp_groupDiff_sameFacet_weiblichSubgroup_2022)[[1]],
+  expect_equal(subset(prepped_dat_berlin, subgroup_var == "male", select = se_mean_comp_groupDiff_sameFacet_femaleSubgroup_2022)[[1]],
                subset(original_dat_berlin,
                       comparison == "groupDiff" &
                         TR_BUNDESLAND == "Berlin" &
                         parameter == "mean" &
-                        Kgender == "maennlich - weiblich" &
+                        Kgender == "female - male" &
                         year == 2022,
                       select = "se"
                )[[1]])
 
-  expect_equal(subset(prepped_dat_berlin, subgroup_var == "weiblich", select = se_mean_comp_groupDiff_sameFacet_maennlichSubgroup_2022)[[1]],
+  expect_equal(subset(prepped_dat_berlin, subgroup_var == "female", select = se_mean_comp_groupDiff_sameFacet_maleSubgroup_2022)[[1]],
                subset(original_dat_berlin,
                       comparison == "groupDiff" &
                         TR_BUNDESLAND == "Berlin" &
                         parameter == "mean" &
-                        Kgender == "maennlich - weiblich" &
+                        Kgender == "female - male" &
                         year == 2022,
                       select = "se"
                )[[1]])
 
 
-  expect_equal(subset(prepped_dat_berlin, subgroup_var == "maennlich", select = p_mean_comp_trend_groupDiff_sameFacet_weiblichSubgroup_2009_2022)[[1]],
+  expect_equal(subset(prepped_dat_berlin, subgroup_var == "male", select = p_mean_comp_trend_groupDiff_sameFacet_femaleSubgroup_2009_2022)[[1]],
                subset(original_dat_berlin,
                       comparison == "trend_groupDiff" &
                         TR_BUNDESLAND == "Berlin" &
                         parameter == "mean" &
-                        Kgender == "maennlich - weiblich" &
+                        Kgender == "female - male" &
                         year == "2022 - 2009",
                       select = "p"
                )[[1]])
 
 
-  expect_equal(subset(prepped_dat_berlin, subgroup_var == "maennlich", select = est_mean_comp_trend_crossDiff_of_groupDiff_totalFacet_weiblichSubgroup_2009_2015)[[1]],
+  expect_equal(subset(prepped_dat_berlin, subgroup_var == "male", select = est_mean_comp_trend_crossDiff_of_groupDiff_totalFacet_femaleSubgroup_2009_2015)[[1]],
                subset(original_dat_berlin,
                       comparison == "trend_crossDiff_of_groupDiff" &
                         TR_BUNDESLAND == "Berlin - total" &
                         parameter == "mean" &
-                        Kgender == "maennlich - weiblich" &
+                        Kgender == "female - male" &
                         year == "2015 - 2009",
                       select = "est"
                )[[1]])
 
-  expect_true(is.na(subset(prepped_dat_berlin, subgroup_var == "total", select = est_mean_comp_trend_crossDiff_of_groupDiff_totalFacet_weiblichSubgroup_2009_2015)[[1]]))
+  expect_true(is.na(subset(prepped_dat_berlin, subgroup_var == "total", select = est_mean_comp_trend_crossDiff_of_groupDiff_totalFacet_femaleSubgroup_2009_2015)[[1]]))
 
 
 })
